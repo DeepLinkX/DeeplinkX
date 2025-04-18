4cbd2274a98cce52b635647d442c9d739405b45a
Parham Hatanian <parham.dev@gmail.com>
2025-04-18 13:56:54 +0330

    Set minimum macOS version to 10.15 Update mac packages

--- example/macos/Flutter/GeneratedPluginRegistrant.swift
+++ example/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,8 +5,10 @@
 import FlutterMacOS
 import Foundation
 
+import deeplink_x_macos
 import url_launcher_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  DeeplinkXMacosPlugin.register(with: registry.registrar(forPlugin: "DeeplinkXMacosPlugin"))
   UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
 }
