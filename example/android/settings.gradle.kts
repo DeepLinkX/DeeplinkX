a93cf94f1861c8a6de51528a425e7e483dc22af3
Parham Hatanian <parham.dev@gmail.com>
2026-02-17 18:57:07 +0330

    Update example project Android and iOS config

--- example/android/settings.gradle.kts
+++ example/android/settings.gradle.kts
@@ -14,12 +14,28 @@ pluginManagement {
         mavenCentral()
         gradlePluginPortal()
     }
+
+    resolutionStrategy {
+        eachPlugin {
+            if (
+                requested.id.id == "com.android.application" ||
+                requested.id.id == "com.android.library" ||
+                requested.id.id == "com.android.test" ||
+                requested.id.id == "com.android.dynamic-feature"
+            ) {
+                useModule("com.android.tools.build:gradle:${requested.version}")
+            }
+            if (requested.id.id == "org.jetbrains.kotlin.android") {
+                useModule("org.jetbrains.kotlin:kotlin-gradle-plugin:${requested.version}")
+            }
+        }
+    }
 }
 
 plugins {
     id("dev.flutter.flutter-plugin-loader") version "1.0.0"
-    id("com.android.application") version "8.7.0" apply false
-    id("org.jetbrains.kotlin.android") version "1.8.22" apply false
+    id("com.android.application") version "8.13.1" apply false
+    id("org.jetbrains.kotlin.android") version "2.2.21" apply false
 }
 
 include(":app")
